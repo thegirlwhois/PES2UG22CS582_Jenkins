a8a6e272d349be50aa007f8e33ab56cd517b4f36
Srushti <srushti.srikanth@gmail.com>
2025-03-13 19:55:38 +0530

    Updated hello.cpp with new changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "I'm Srushti, PES2UG22CS582";
-
+  cout << "I'm Srushti, PES2UG22CS582\n";
+  cout << "I'm sleepy... zzz\n";
+  cout <<"oh no, i woke up";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
 
